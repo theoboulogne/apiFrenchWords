8b0f71ee36652e0b19a50a53f60368a0332c8ae6
theoboulogne <58325434+theoboulogne@users.noreply.github.com>
2021-11-04 09:35:54 +0100

    qzqd

--- api.js
+++ api.js
@@ -1,4 +1,12 @@
 const PORT = process.env.PORT || 3000;
+
+const express = require('express')
+const app = express()
+
 app.listen(PORT, () => {
   console.warn(`App listening on http://localhost:${PORT}`);
 });
+
+app.get('/mots', (req,res) => {
+    res.status(200).json(["test"])
+})
